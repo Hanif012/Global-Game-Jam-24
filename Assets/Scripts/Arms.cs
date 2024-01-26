c741ea7867d1d41a89b32c4dbc444da4df4942b9
Steven111105 <stevenwijaya1111@gmail.com>
2024-01-26 23:07:44 +0700

    GRAB FINALLY DONE
    Cape cok anj g ngerti lg gw anj salah mulu gw salah saya dimana kalo saya ngoding 5 jam tanpa video :cry:

---
+++ Assets/Scripts/Arms.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arms : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    int speed = 300;
+    public KeyCode key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerpos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        Vector2 difference = new Vector2(playerpos.x - transform.position.x, playerpos.y - transform.position.y);
+        float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        if(Input.GetKey(key))
+        {
+            rb.MoveRotation(Mathf.LerpAngle(rb.rotation, rotation, speed * Time.fixedDeltaTime));   
+        }  
+
+        
+    }
+}
--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class Grab : MonoBehaviour
 {
-    // private void OnCollisionStay2D(Collision2D other)
-    // {
-    //     Debug.Log("grabbed");
-    //     if(Input.GetMouseButtonDown(0)){
-    //         this.GetComponent<HingeJoint2D>().connectedBody = other.gameObject.GetComponent<Rigidbody2D>();
-    //         this.GetComponent<HingeJoint2D>().enabled = true;
-    //     }
-    // }
-
-    // private void OnCollisionExit2D(Collision2D other)
-    // {
-    //     Debug.Log("exit");
-    //     this.GetComponent<HingeJoint2D>().enabled = false;
-    // }
+    private bool hold;
+    public KeyCode key;
+    private void Update()
+    {
+        if(Input.GetKey(key))
+        {
+            // Debug.Log("pressed");
+            hold = true;
+        }else{
+            hold = false;
+            Destroy(GetComponent<FixedJoint2D>());
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(!other.transform.CompareTag("Player") && !other.transform.CompareTag("Ground"))
+        {
+            Debug.Log(hold);
+            if(hold)
+            {
+                Debug.Log("grabbed");
+                Rigidbody2D rb = other.transform.GetComponent<Rigidbody2D>();
+                Debug.Log(rb);
+                if(rb != null){
+                    FixedJoint2D fj = gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+                    fj.connectedBody = rb;
+                }else{
+                    FixedJoint2D fj = gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+                }
+            }
+        }
+    }
 }
