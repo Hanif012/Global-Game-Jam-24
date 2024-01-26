18b7ae8a2f48a1452a88bd118a389d7224994385
GeezNuts <georgesuitela05@gmail.com>
2024-01-26 20:31:00 +0700

    prototype-char
    it wok but need upgrade

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   
+    public Rigidbody2D rb;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 10f;
+    public bool isJumpPressed = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           isJumpPressed = true;
+           Debug.Log("Update Jump");
+         } 
+    }
+
+    void FixedUpdate()
+    {
+        if (isJumpPressed)
+        {
+            Debug.Log("FixedUpdate Jump");
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);   
+            isJumpPressed = false;
+        }
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+    }
+}
