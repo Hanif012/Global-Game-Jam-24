169e9025f3e07db988339c4eaefa2c4a9bb71ec4
Steven111105 <stevenwijaya1111@gmail.com>
2024-01-26 19:45:39 +0700

    Grab Test
    Lelah cape cape banget g ngerti

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    // private void OnCollisionStay2D(Collision2D other)
+    // {
+    //     Debug.Log("grabbed");
+    //     if(Input.GetMouseButtonDown(0)){
+    //         this.GetComponent<HingeJoint2D>().connectedBody = other.gameObject.GetComponent<Rigidbody2D>();
+    //         this.GetComponent<HingeJoint2D>().enabled = true;
+    //     }
+    // }
+
+    // private void OnCollisionExit2D(Collision2D other)
+    // {
+    //     Debug.Log("exit");
+    //     this.GetComponent<HingeJoint2D>().enabled = false;
+    // }
+}
