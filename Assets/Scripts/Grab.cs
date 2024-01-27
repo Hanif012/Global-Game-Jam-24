dea576825dccd626f793a5b3f2d1a3a4e36d57af
Hanif <andque12345012@gmail.com>
2024-01-27 11:26:15 +0700

    tukeran job sama steven

--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -8,32 +8,48 @@ public class Grab : MonoBehaviour
     public KeyCode key;
     private void Update()
     {
-        if(Input.GetKey(key))
+        if (Input.GetKey(key))
         {
             // Debug.Log("pressed");
             hold = true;
-        }else{
+        }
+        else
+        {
             hold = false;
             Destroy(GetComponent<FixedJoint2D>());
         }
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if(!other.transform.CompareTag("Player") && !other.transform.CompareTag("Ground"))
+        if (!other.transform.CompareTag("Player") && !other.transform.CompareTag("Ground"))
         {
             Debug.Log(hold);
-            if(hold)
+            if (hold)
             {
                 Debug.Log("grabbed");
                 Rigidbody2D rb = other.transform.GetComponent<Rigidbody2D>();
                 Debug.Log(rb);
-                if(rb != null){
+                if (rb != null)
+                {
                     FixedJoint2D fj = gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
                     fj.connectedBody = rb;
-                }else{
+                }
+                else
+                {
                     FixedJoint2D fj = gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
                 }
             }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+        if(!collision.transform.CompareTag("Player") && !collision.transform.CompareTag("Ground") && !hold)
+        {
+            Debug.Log("exit");
+            //not working 
+            SoundManager.PlaySound(SoundManager.Sound.PlayerThrow);
         }
     }
 }
