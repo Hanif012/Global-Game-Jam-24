3731ba0a8dab7479f66994640e186857049ba968
GeezNuts <georgesuitela05@gmail.com>
2024-01-27 13:11:46 +0700

    new mc + googly eye
    it wok

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 10f;
     public bool isJumpPressed = false;
+    [SerializeField] bool grounded;
 
     void Start()
     {
@@ -18,7 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
            isJumpPressed = true;
            Debug.Log("Update Jump");
@@ -34,8 +35,19 @@ public class PlayerController : MonoBehaviour
             isJumpPressed = false;
         }
         PlayerMovement();
+        
     }
-
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if(other.tag =="Ground"&& !grounded)
+        {
+            grounded=true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        grounded=false;
+    }
     void PlayerMovement()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
