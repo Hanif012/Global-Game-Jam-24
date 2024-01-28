90327fd451a1b1c365f79af01d57ad6e4c827e5f
Steven111105 <stevenwijaya1111@gmail.com>
2024-01-28 13:33:42 +0700

    Made some levels

--- Assets/Scripts/Dart.cs
+++ Assets/Scripts/Dart.cs
@@ -5,28 +5,18 @@ using UnityEngine;
 public class Dart : MonoBehaviour
 {
     public SpawnManager spawnManager;
-    // Start is called before the first frame update
-    void Start()
-    {
-        spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("yellowballoon"))
         {
+            spawnManager.PopSFX();
             Destroy(other.gameObject);
-            spawnManager.counterYellow--;
+            spawnManager.counterYellow++;
         }
         if(other.CompareTag("orangeballoon"))
         {
+            spawnManager.PopSFX();
             Destroy(other.gameObject);
         }
         if(other.CompareTag("DartDelete"))
